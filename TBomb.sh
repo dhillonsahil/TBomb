4a1a5dbe28ccab10cb4f217638741ee80f8aa6f9
dhillonsahil <60694942+dhillonsahil@users.noreply.github.com>
2021-08-30 02:39:55 +0000

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -35,7 +35,7 @@ banner() {
         echo -e "\e[4;34m This Bomber Was Created By \e[1;32mDhillon Sahil \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border Dhillonsahil
+        toilet -f mono12 -F border Dhillon sahil
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
     echo -e "\e[1;32m           Telegram: https://t.me/BombattackChat \e[0m"
